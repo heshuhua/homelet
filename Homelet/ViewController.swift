37028ef41d9ccac02122a764ea0aaea58b34ce16
heshuhua <heshuhua@gmail.com>
2018-02-07 15:23:43 +0800

    Initial Commit

---
+++ Homelet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homelet
+//
+//  Created by heshuhua on 2018/2/7.
+//  Copyright © 2018年 heshuhua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
